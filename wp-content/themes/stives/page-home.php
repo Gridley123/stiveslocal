753a41939f911a8d81b8c1006ca33b6a531007f5
GEORGE RIDLEY <gridley123@hotmail.com>
2016-06-02 22:52:19 +0100

    Adapted css and header, created page-home.php

--- wp-content/themes/stives/page-home.php
+++ wp-content/themes/stives/page-home.php
@@ -1,6 +1,10 @@
-<?php get_header(); ?>
+
+<?php 
+/*
+  Template Name: Home Page
+ */
+     get_header(); ?>
 <div id="page-content">
 	<?php get_template_part('loop', 'home'); ?>
 </div>
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
