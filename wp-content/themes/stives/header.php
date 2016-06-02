565deea306751c416792b5162bf09016779a27a7
GEORGE RIDLEY <gridley123@hotmail.com>
2016-06-02 17:56:51 +0100

    Added optimized.min.js to enqued scripts, setup requirejs, added bootstrap SASS to require and global.scss.  Initialized navbar and new menu

---
+++ wp-content/themes/stives/header.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7" <?php language_attributes() ?>><![endif]-->
+<!--[if IE 7]><html class="no-js lt-ie9 lt-ie8" <?php language_attributes() ?>><![endif]-->
+<!--[if IE 8]><html class="no-js lt-ie9" <?php language_attributes() ?>><![endif]-->
+<!--[if gt IE 8]><!--><html class="no-js" <?php language_attributes() ?>><!--<![endif]-->
+    <head>
+        <meta charset="<?php bloginfo( 'charset' ) ?>">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta name="viewport" content="width=device-width">
+        <title><?php wp_title( '|', true, 'right' ) ?></title>
+		<meta name="author" content="">
+		<link rel="author" href="">
+		<?php wp_head() ?>
+    </head>
+    <body <?php body_class() ?>>
+  <!-- === HEADER SECTION (BRAND, NAVBAR) {{{2 === -->
+
+      <header class="site-header" role="banner">
+
+        <nav class="navbar navbar-default" role="navigation">
+          <!-- Brand and toggle get grouped for better mobile display -->
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+
+          </div>
+
+        <?php wp_nav_menu(array(
+                'theme_location' => 'main-nav',
+                'container'      => 'div',
+                'container_class'   => 'collapse navbar-collapse navbar-ex1-collapse',
+                'menu_class' => 'nav navbar-nav',
+        )) ?>
+
+      </header>
+		<div id="content-wrap">
---
+++ wp-content/themes/stives/js/global.js
@@ -0,0 +1,13 @@
+require.config({
+	baseUrl: "wp-content/themes/stives/js",
+	paths: {
+		jquery: "vendor/jquery/jquery",
+                bootstrap: "vendor/bootstrap-sass/assets/javascripts/bootstrap"
+	}
+});
+
+require(['jquery', 'bootstrap'], function($) {
+	console.log('Working!!');
+});
+
+
