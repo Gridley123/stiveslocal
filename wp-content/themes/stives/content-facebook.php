8a213bfba8fc64c8d2e8d9a772d2946794ff1fee
GEORGE RIDLEY <gridley123@hotmail.com>
2016-06-19 16:52:17 +0100

    Added custom facebook feed plugin and adjusted settings

--- wp-content/themes/stives/content-facebook.php
+++ wp-content/themes/stives/content-facebook.php
@@ -1,11 +1,19 @@
-
+<?php $facebook_feed = get_field('facebook_feed'); ?>
         <!-- === FACEBOOK FEED SECTION {{{3 === -->
 
         <section class="facebook">
           <div class="container">
-            <i class="fa fa-facebook fa-3x"></i>
+            <div class="icon">
+                <i class="fa fa-facebook fa-3x"></i>
+            </div>
             <div class="fbplugin">
-              <img src="<?php bloginfo('stylesheet_directory'); ?>/images/facebookfeed1.png" alt="">
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-2">
+                        <div class="facebook-feed">
+                            <?php echo $facebook_feed;?>
+                        </div>
+                    </div>
+                </div>
             </div><!-- /.fbplugin -->
           </div><!-- /.container -->
         </section><!-- /.facebook -->
--- wp-content/themes/stives/page-advertise.php
+++ wp-content/themes/stives/page-advertise.php
@@ -4,6 +4,15 @@
      */
 ?>
 <?php get_header() ?>
-<?php get_template_part('content', 'adtext') ?>
+if ( have_posts() ) {
+    while ( have_posts() ) {
+        the_post();
+        the_content();    
+    }
+} else {
+    
+}
+
+wp_reset_postdata();
 <?php get_footer() ?>
 
